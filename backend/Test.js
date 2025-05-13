921139cbd76c5e6549d0056c35923599054d9a87
Malavika <mals190399@gmail.com>
2025-05-13 20:02:33 +0530

    done

--- backend/Test.js
+++
@@ -1,8 +0,0 @@
-// {
-//     "name": "Sweet 1",
-//     "image": "Pic",
-//     "price": 23,
-//     "description": "Sweet Desc",
-//     "ingredients": ["A", "B", "C"],
-//     "category": "Catergory A"
-//   }
